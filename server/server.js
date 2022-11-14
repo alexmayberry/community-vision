25c4339997b796677d061258811db231d8d6dede
JungHan Seo <junghanseo84@gmail.com>
2022-11-14 14:59:14 -0800

    modifying imageUpload data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const cors = require('cors')
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
